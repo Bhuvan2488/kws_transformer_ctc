3e01a7fea4f924d8779d42ee847ff826665d8d87
Bhuvan Ummidisetti <ubhuvan533@gmail.com>
2026-01-19 19:52:59 +0530

    Update scheduler.py

--- src/training/scheduler.py
+++ src/training/scheduler.py
@@ -1,10 +1,1 @@
-def build_scheduler(optimizer, warmup_steps: int = 0):
-    if warmup_steps <= 0:
-        return None
 
-    def lr_lambda(step):
-        return min((step + 1) / warmup_steps, 1.0)
-
-    return __import__("torch").optim.lr_scheduler.LambdaLR(
-        optimizer, lr_lambda
-    )
