bcdb4f0b06a778184d2597f9408b5da26f1ce24a
Bhuvan2488 <bhuvan.spirelab.iisc@gmail.com>
2026-01-21 13:23:56 +0530

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,1 +1,16 @@
+import sys
+from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+sys.path.append(str(PROJECT_ROOT))
+
+from src.training.train import train
+
+
+def main():
+    print("🚀 Launching training pipeline...")
+    train()
+
+
+if __name__ == "__main__":
+    main()
